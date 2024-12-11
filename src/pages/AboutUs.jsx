52d71e3965fed52c7e60bce39adf01f4749c3246
wheelyu <harun.abdulkarim17@gmail.com>
2024-12-11 14:25:09 +0700

    Update AboutUs.jsx

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import AboutUs from "../components/AboutUs/aboutus";
+import AboutUs from "../components/AboutUs/story";
 import StickyCtaButton from "../components/stickyCtaButton";
 export default function Product() {
     return (
