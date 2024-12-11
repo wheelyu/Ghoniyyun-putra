e938866d94c054b188cb7ccbcfc18e7d06974468
wheelyu <harun.abdulkarim17@gmail.com>
2024-12-11 14:24:17 +0700

    Update AboutUs.jsx

--- src/components/Home/AboutUs.jsx
+++ src/components/Home/AboutUs.jsx
@@ -34,19 +34,23 @@ const About = () => {
     ];
 
     return (
-        <div className=" bg-gradient-to-r from-primary to-pink-500 bg-opacity-85 py-32 px-4 h-fit w-full mx-auto flex flex-col md:flex-row items-center justify-center">
+        <div className=" bg-gradient-to-r from-primary to-pink-500 bg-opacity-85 py-32 px-4 h-fit w-full mx-auto flex flex-col  items-center justify-center">
+             <div className="w-full md:w-1/3 mt-8 md:mt-0">
+                <h1 className="text-7xl font-bold text-center text-white">
+                    Why Choose Us?
+                </h1>
+            </div>
             <div 
-                className="grid grid-cols-1 gap-8 w-full md:w-1/3 md:pr-12 items-center"
+                className="grid grid-cols-3 gap-8 w-10/12 mt-12 items-center"
             >
                 {cardData.map((card, index) => (
                 <div 
                     key={index} 
                     className="bg-white shadow-lg rounded-lg p-6 flex flex-row items-center h-[200px]
                             transition-all duration-300 ease-in-out 
-                            border-s-[10px] hover:border-primary 
+                            border-b-[10px] hover:border-primary 
                             hover:shadow-xl hover:cursor-pointer"
-                    data-aos="fade-up"
-                    data-aos-delay={index * 400}
+                        
                 >
                     <FontAwesomeIcon 
                     icon={card.icon} 
@@ -62,11 +66,7 @@ const About = () => {
                 ))}
             </div>
             
-            <div className="w-full md:w-1/3 mt-8 md:mt-0">
-                <h1 className="text-7xl font-bold text-center text-white">
-                    Why Choose Us?
-                </h1>
-            </div>
+           
         </div>
 
     );
