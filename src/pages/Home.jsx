74537533ce53d797cb5f7cec89c72859ac0ca900
wheelyu <harun.abdulkarim17@gmail.com>
2024-12-11 14:25:15 +0700

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,34 @@
-import Navbar from "../components/Navbar";
+import React, { useRef } from "react";
 import Profil from "../components/Home/Profil";
 import AboutUs from "../components/Home/AboutUs";
 import Product from "../components/Home/product";
 import Footer from "../components/Footer";
 import Hero from "../components/Home/Hero";
+import Founder from "../components/Home/Founder";
+import Story from "../components/AboutUs/story";
+import Partnership from "../components/Home/Partnership";
 import StickyCtaButton from "../components/stickyCtaButton";
+import ContactForm from "../components/Contact/ContactForm";
 export default function Home() {
+  const section1Ref = useRef(null);
+  const contactRef = useRef(null);
+
+  const scrollToSection = (sectionRef) => {
+    sectionRef.current.scrollIntoView({ behavior: 'smooth' });
+  };
   return (
     <div>
       <Hero/>
-      <Profil />
+      <Profil scrollToSection={scrollToSection} contactRef={contactRef}/>
       <AboutUs />
+      <Partnership />
       <Product />
+      <Founder />
+      <Story />
+      <div ref={contactRef}>
+      
+      <ContactForm /> 
+      </div>
       <Footer />
       <StickyCtaButton />
     </div>
