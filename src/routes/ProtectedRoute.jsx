e3a07611ee1a5835bd9c9820a9a07c80750f9ccf
wheelyu <harun.abdulkarim17@gmail.com>
2025-01-06 02:11:15 +0700

    Update ProtectedRoute.jsx

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -1,7 +1,8 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router";
+import useAuthStore from "../stores/authStore";
 
 const ProtectedRoute = () => {
-    const  token  = localStorage.getItem('token');
+    const { token } = useAuthStore();
 
     if (!token) {
         return <Navigate to={"/login"} replace />;
