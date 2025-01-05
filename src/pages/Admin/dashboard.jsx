75144c852762052300147e0d6d466f22dba3dc16
wheelyu <harun.abdulkarim17@gmail.com>
2025-01-06 02:11:28 +0700

    Update dashboard.jsx

--- src/pages/Admin/dashboard.jsx
+++ src/pages/Admin/dashboard.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Sidebar from "../../components/Admin/sidebar";
 import Header from "../../components/Admin/Header";
+import useAuthStore from "../../stores/authStore";
 const Dashboard = () => {
     return <div className="flex h-screen">
-        <Sidebar />
+        <Sidebar active="Dashboard" />
         <div className="flex-1 flex flex-col">
             <Header />
             <main className="flex-1 p-6 bg-gray-100">
-            <h1 className="text-2xl font-bold mb-4">Selamat Datang di Dasbor Admin</h1>
+            <h1 className="text-2xl font-bold mb-4">Welcome to Dashboard Admin</h1>
           {/* Isi konten utama akan ditambahkan di sini */}
         </main>
         </div>
