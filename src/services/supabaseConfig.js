386f443785e69396e96cf805b2dfbc040c22b507
wheelyu <harun.abdulkarim17@gmail.com>
2025-01-06 02:11:11 +0700

    Create supabaseConfig.js

---
+++ src/services/supabaseConfig.js
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js';
+
+// Mengambil variabel dari .env
+const supabaseUrl = "https://cacsbnrslwosiditepop.supabase.co"
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+// Membuat client Supabase
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
