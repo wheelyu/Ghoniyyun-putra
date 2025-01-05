f0fe325ed44e9f8cd6694349568ea3034cd189fd
wheelyu <harun.abdulkarim17@gmail.com>
2025-01-06 02:11:47 +0700

    Update Sidebar.jsx

--- src/components/Admin/Sidebar.jsx
+++ src/components/Admin/Sidebar.jsx
@@ -1,97 +1,120 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import useAuthStore from '../../stores/authStore';
+import { logoutAlert, Toast } from "../alert/toast";
 import { 
     Home, 
+    Power,
     Users,
     BoxIcon,
     ListCheckIcon,
     ChevronLeft, 
-    ChevronRight, 
-  } from 'lucide-react';
+    ChevronRight,
+    Settings
+} from 'lucide-react';
 import { Link } from "react-router-dom";
-const Sidebar = () => {
+const Sidebar = ({ active }) => {
+    const navLink = [
+        {
+            path: "/admin/dashboard",
+            name: "Dashboard",
+            icon: <Home />,
+        },
+        {
+            path: "/admin/product",
+            name: "Product",
+            icon: <BoxIcon />,
+        },
+        {
+            path: "/admin/category",
+            name: "Category",
+            icon: <ListCheckIcon />,
+        },
+        {
+            path: "/admin/client",
+            name: "Client",
+            icon: <Users />,
+        },
+    ]
     const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-
+    const navigate = useNavigate();
+    const { clearToken } = useAuthStore();
     const handleLogout = () => {
-        localStorage.removeItem('token');
-        window.location.href = '/';
-    }
+        const logoutAction = () => {
+                // Clear the authentication token
+                clearToken();
+    
+                // Redirect to login page
+                navigate('/');
+                
+                // Show success message
+                Toast.fire({
+                    icon: "success",
+                    title: "Logout success.",
+                });
+                // Close dropdown
+                };
+            
+                // Show logout confirmation alert
+                logoutAlert(logoutAction);
+        };
     return (
         <div className="flex">
             <div className={`
                 ${isSidebarOpen ? 'w-64' : 'w-20'} 
-                bg-gray-800 text-white 
+                bg-primary text-white 
                 transition-all duration-300 
                 flex flex-col relative
             `}>
                 {/* Toggle Sidebar Button */}
                 <button 
                 onClick={() => setIsSidebarOpen(!isSidebarOpen)}
-                className="absolute top-4 right-4 z-10 bg-gray-700 p-1 rounded-full"
+                className="absolute top-4 right-4 z-10 bg-primary p-1 rounded-full"
                 >
                 {isSidebarOpen ? <ChevronLeft /> : <ChevronRight />}
                 </button>
 
                 {/* Logo */}
-                <div className=" flex p-5 h-20  border-b border-gray-700">
+                <div className=" flex p-5 h-20  border-b border-white">
+                    
                 <h2 className={`
-                    ${isSidebarOpen ? 'block' : 'hidden'} 
+                    ${isSidebarOpen ? 'flex gap-2' : 'hidden'} 
                     text-xl font-bold
                 `}>
+                    <Settings className="w-8 h-8" />
                     Admin Panel
                 </h2>
                 </div>
 
                 {/* Menu Items */}
-                <nav className="mt-8 flex-1">
-                    <Link to="/admin/dashboard">
-                        <div 
-                        className="px-4 py-3 hover:bg-gray-700 cursor-pointer flex items-center"
-                        >
-                        
-                        <Home className="w-5 h-5" />
-                        
-                        <span className={`
-                            ${isSidebarOpen ? 'ml-3 block' : 'hidden'}
-                            text-sm
-                        `}>
-                            Dashboard
-                        </span>
-                        </div>
+                <nav className="mt-8 ml-3 mr-3 flex-1 ">
+                {navLink.map((item, index) => (
+                    <Link
+                    key={index}
+                    to={item.path}
+                    className={`
+                        flex items-center text-sm py-2 px-4 font-medium text-gray-100 rounded-md mt-2
+                        hover:bg-secondary cursor-pointer
+                        ${active === item.name ? 'bg-secondary' : ''}
+                    `}
+                    >
+                    {item.icon}
+                    <span className={`
+                        ${isSidebarOpen ? 'ml-3 block' : 'hidden'}
+                        text-sm
+                    `}>
+                        {item.name}
+                    </span>
                     </Link>
-
-                    <Link to="/admin/kategori">
-                        <div 
-                        className="px-4 py-3 hover:bg-gray-700 cursor-pointer flex items-center"
-                        >
-                        <ListCheckIcon className="w-5 h-5" />
-                        <span className={`
-                            ${isSidebarOpen ? 'ml-3 block' : 'hidden'}
-                            text-sm
-                        `}>
-                            Category
-                        </span>
-                        </div>
-                    </Link>
-
-                    <Link to="/admin/produk">
-                        <div
-                        className="px-4 py-3 hover:bg-gray-700 cursor-pointer flex items-center"
-                        >
-                        <BoxIcon className="w-5 h-5" />
-                        <span className={`
-                            ${isSidebarOpen ? 'ml-3 block' : 'hidden'}
-                            text-sm
-                        `}>
-                            Products
-                        </span>
-                        </div>
-                    </Link>
+                    
+                ))}
                 </nav>
 
                 {/* Footer */}
-                <div className="flex-1" />
+                <div className="flex-1 " />
                     <button onClick={handleLogout}>
-                        <div className="px-4 py-3 hover:bg-gray-700 cursor-pointer flex items-center">
+                        <div className="px-4 py-3 ml-3 mr-3 hover:bg-secondary cursor-pointer flex items-center">
+                            <Power className="w-5 h-5" />
                             <span className={`
                                 ${isSidebarOpen ? 'ml-3 block' : 'hidden'}
                                 text-sm
