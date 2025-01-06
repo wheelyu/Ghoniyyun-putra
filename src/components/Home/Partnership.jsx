a9b508ecd39da0abbe7289b55c47b644533afa01
wheelyu <harun.abdulkarim17@gmail.com>
2025-01-06 23:21:45 +0700

    Update Partnership.jsx

--- src/components/Home/Partnership.jsx
+++ src/components/Home/Partnership.jsx
@@ -1,17 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Pertamina from "../../assets/Pertamina_Logo.png";
+import Marquee from "react-fast-marquee";
+import { supabase } from "../../services/supabaseConfig";
 const Partnership = () => {
+    const [partnerImages, setPartnerImages] = useState([]);
+
+    const fetchPartnerImages = async () => {
+        const { data, error } = await supabase
+            .from("partnership")
+            .select("image_url")
+            .order("id", { ascending: true })
+            .eq("is_active", true);
+        setPartnerImages(data);
+    };
+
+    useEffect(() => {
+        fetchPartnerImages();
+    }, []);
     return (
-        <div className=" py-10">
+        <div className="py-10">
             <div className="container mx-auto px-4">
-                <h2 className="text-5xl font-bold text-primary mb-4 mx-auto text-center">Partnership</h2>
-                <div className="py-10 flex flex-row">
-                    <img src={Pertamina} alt="Pertamina" className="w-[300px] mx-auto" />
-                    <img src={Pertamina} alt="Pertamina" className="w-[300px] mx-auto" />
-                    <img src={Pertamina} alt="Pertamina" className="w-[300px] mx-auto" />
-                    <img src={Pertamina} alt="Pertamina" className="w-[300px] mx-auto" />
+                <h2 className="text-5xl font-bold text-primary mb-4 mx-auto text-center">
+                    Partnership
+                </h2>
+                <div className="py-10">
+                    <Marquee speed={50} gradient={false}>
+                        {partnerImages.map((image, index) => (
+                            <img
+                                key={index}
+                                src={image.image_url}
+                                alt={`Partner ${index + 1}`}
+                                className="w-40 h-40 object-contain"
+                            />
+                        ))}
+                        
+                    </Marquee>
                 </div>
-            </div>  
+            </div>
         </div>
     );
 };
