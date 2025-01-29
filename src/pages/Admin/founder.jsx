722039c47890b504724783ac80a15ebc45fc4338
wheelyu <harun.abdulkarim17@gmail.com>
2025-01-29 15:48:15 +0700

    Update founder.jsx

--- src/pages/Admin/founder.jsx
+++ src/pages/Admin/founder.jsx
@@ -78,7 +78,7 @@ const Founder = () => {
         
     return (
         <div className="flex min-h-screen">
-            <Sidebar active="Link" />
+            <Sidebar active="Founder" />
             <div className="flex-1 flex flex-col">
                 <Header />
                 <main className="flex-1 p-6 bg-gray-100">
